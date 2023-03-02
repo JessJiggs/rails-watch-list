79e91beff8f27802dffb1eea9813ab02f86fdf9b
JessJiggs <verheuljess@gmail.com>
2023-03-02 14:38:12 +0200

    dependencies and uniqueness and scope associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# resources user-movies
+# - index & show
+# - create & new
+#   resources movie-list
+#     - index
+#     - bookmark
+#     - destroy a bookmark
+
+#     resources :movies, only: [:index, :show] do
+#       resources :bookmark, only: [:index, :new, :create]
+#     end
 
   # Defines the root path route ("/")
   # root "articles#index"
